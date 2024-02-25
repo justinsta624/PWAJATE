d158f8318fcd270859db75b7fc2b348df7ebd8b3
justinsta624 <justinsta624@gmail.com>
2024-02-24 19:19:06 -0500

    Update webpack.config.js
    - Add and configure plugins for service worker and manifest file
    - Add CSS loaders and babel to webpack

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,3 +1,4 @@
+// Import necessary plugins and modules
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
@@ -6,24 +7,91 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
 // TODO: Add CSS loaders and babel to webpack.
 
+// Import additional plugins
+const WorkboxPlugin = require("workbox-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// Export the configuration as a function
 module.exports = () => {
   return {
+    // Set the development mode
     mode: 'development',
+
+    // Define entry points for the application
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+
+    // Specify the output configuration
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+
+    // Configure plugins for Webpack
     plugins: [
-      
+      // Generate HTML file with injected bundles
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Webpack Plugin",
+      }),
+
+      // Extract CSS into separate files
+      new MiniCssExtractPlugin(),
+
+      // Inject the service worker manifest
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+
+      // Configure PWA manifest settings
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: "Just Another Text Editor",
+        short_name: "J.A.T.E",
+        description: "offline text editor",
+        background_color: "#225ca3",
+        theme_color: "#225ca3",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons"),
+          },
+        ],
+      }),
     ],
 
+    // Configure module rules
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: [
+                "@babel/plugin-proposal-object-rest-spread",
+                "@babel/transform-runtime",
+              ],
+            },
+          },
+        },
       ],
     },
   };
